5947a57baa3463d8713e7e33759ca6b9119c39dd
nagaiyume-pg <na9a1yume.pg@gmail.com>
2025-02-19 19:07:47 +0900

    chore: 全て削除

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: [['@babel/preset-env', 'babel-preset-expo', {targets: {node: 'current'}}]],
-    plugins: [
-      ['babel-plugin-react-docgen-typescript', { exclude: 'node_modules' }],
-    ],
-  };
-};
--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-// prettier.config.js
-module.exports = {
-  semi: true, // セミコロンを使用
-  singleQuote: true, // シングルクォートを使用
-  trailingComma: 'es5', // 配列やオブジェクトの最後にカンマを追加
-  tabWidth: 2, // インデントの幅
-  printWidth: 80, // 1行の最大文字数
-  endOfLine: 'lf', // 改行コード
-};
