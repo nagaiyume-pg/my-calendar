451f65bebe257bd30509dd996f5cf0ae1fe67ac6
nagaiyume-pg <na9a1yume.pg@gmail.com>
2025-02-19 18:51:59 +0900

    update

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [['@babel/preset-env', 'babel-preset-expo', {targets: {node: 'current'}}]],
     plugins: [
       ['babel-plugin-react-docgen-typescript', { exclude: 'node_modules' }],
     ],
