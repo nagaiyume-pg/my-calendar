f70b9dca411a3099aaadf9f56aa402414cdff118
nagaiyume-pg <na9a1yume.pg@gmail.com>
2025-03-03 18:07:00 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { ScheduleScreen } from './screens';
 
 const App = () => {
   return(
     <SafeAreaProvider>
-      <ScheduleScreen />
     </SafeAreaProvider>
   )
 }
--- src/screens/index.ts
+++
@@ -1,1 +0,0 @@
-export { ScheduleScreen } from "./Schedule";
