d6f4abd63af9ca4b5f3d2d5863abfdd3d1ffa4fb
nagaiyume-pg <na9a1yume.pg@gmail.com>
2025-03-03 18:52:07 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
+}
+
+let AppEntryPoint = App;
+
+if (process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === "true") {
+  AppEntryPoint = require("../.storybook").default;
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +24,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default AppEntryPoint;
